bb5fb848d2b0c9102af0f96e3bbdd303ecf0054c
Vladimir Antonov <v.antonov@ndmsystems.com>
2016-04-08 15:54:37 +0300

    v1.2.0 compile exception error contains 'source' text of function body

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function Ejs(options) {
 	if (!options)
 		options = {};
 
-	var code = [],
+	var empty_code = '"use strict";\nvar $ = "";\n',
+	    code = empty_code,
 	    text_mode = false,
 
 	    open_str = options.open_str || '<?',
@@ -22,29 +23,30 @@ function Ejs(options) {
 	    global_id = options.global_id || 'global',
 
 	    push_expr = /*this.push_expr = */function _push_expr() {
-			code.push(text_mode ? ", " : "$.push(");
-			code.push.apply(code, arguments);
+			code += text_mode ? "+(" : "$+=(";
+			for (var i = 0, a = arguments, n = a.length; i < n; ++i)
+				code += a[i];
+			code += ")";
 			text_mode = true;
 			return this;
 		},
 
 	    push_string = /*this.push_string = */function _push_string(str) {
-			code.push(
-				!text_mode ? "$.push('" : ", '",
-				str.replace(/[\\'\n]/g, /*'*/function (match) {
+			code += !text_mode ? "$+='" : "+'";
+			code += str.replace(/[\\'\n]/g, /*'*/function (match) {
 					return match === '\n' ? "\\n\\\n" : '\\'+match;
-				}),
-				"'");
+				});
+			code += "'";
 			text_mode = true;
 			return this;
 		},
 
 	    push_code = this.push_code = function _push_code() {
 			if (text_mode)
-				code.push(");\n");
+				code += ";\n";
 			text_mode = false;
-			if (arguments.length)
-				code.push.apply(code, arguments);
+			for (var i = 0, a = arguments, n = a.length; i < n; ++i)
+				code += a[i];
 			return this;
 		};
 
@@ -71,10 +73,10 @@ function Ejs(options) {
 				js_end = len;
 			switch (tpl.charAt(js_pos)) {
 			case '=':
-				push_expr(tpl.slice(js_pos + 1, js_end), "+''");
+				push_expr(tpl.slice(js_pos + 1, js_end));
 				break;
 			case '.':
-				push_expr(global_id, '.', tpl.slice(js_pos + 1, js_end), "+''");
+				push_expr(global_id, '.', tpl.slice(js_pos + 1, js_end));
 				break;
 			default:
 				push_code(tpl.slice(js_pos, js_end).trim(), '\n');
@@ -92,13 +94,18 @@ function Ejs(options) {
 			return this;
 		},
 	    listing = this.listing = function _listing() {
-			return code.join('');
+			return code;
 		},
 	    compile = this.compile = function _compile(args) {
-			code.unshift("var $=[];\n");
-			push_code("return $.join('');\n");
-			var f = new Function(args || '', listing());
-			code = [];
+			push_code(";return $;\n");
+			var src = listing();
+			try {
+				var f = new Function(args || '', src);
+			} catch (e) {
+				e.source = src;
+				throw e;
+			}
+			code = empty_code;
 			return f;
 		};
 
--- test/test.js
+++ test/test.js
@@ -1,88 +1,13 @@
 var Ejs = require('../index.js'),
+    json = require('nano-json'),
+    timer = require('nano-timer'),
     assert = require('core-assert');
-
-var timer = function (ms, v) {
-	return new Promise(function (resolve, reject) {
-		var to = setTimeout(function () {
-				resolve(v);
-			}, ms);
-		return { cancel: function () {
-			clearTimeout(to);
-		}};
-	});
-};
-
-function ts(a, radix, deep) {
-	switch (typeof a) {
-	case 'object':
-		if (a instanceof Array) {
-			var o = [];
-			for (var i = 0, n = a.length; i < n; ++i)
-				o[i] = ts(a[i], radix, 1);
-			return deep ? '['+o.join(',')+']' : o.join(',');
-		} else {
-			if (a === null)
-				return 'null';
-			var o = [];
-			for (var id in a)
-				o.push(id+':'+ts(a[id], radix, 1));
-			return '{' + o.join(',') + '}';
-		}
-		break;
-	case 'string':
-		var qc = 0, dqc = 0;
-		for (var i = 0, n = a.length; i < n; ++i)
-			switch (a.charAt(i)) {
-			case "'": ++qc; break;
-			case '"': ++dqc; break;
-			}
-		if (qc <= dqc) {
-			return '"' + a.replace(/["\t\n\r]/g, function (m) { //"
-				switch (m) {
-				case '"':	return '\\"';
-				case '\t':  return '\\t';
-				case '\n':  return '\\n';
-				case '\r':  return '\\r';
-				default:    return m;
-				}
-			}) + '"';
-		} else {
-			return "'" + a.replace(/['\t\n\r]/g, function (m) { //'
-				switch (m) {
-				case "'":	return "\\'";
-				case '\t':  return '\\t';
-				case '\n':  return '\\n';
-				case '\r':  return '\\r';
-				default:    return m;
-				}
-			}) + "'";
-		}
-	case 'number':
-		switch (radix) {
-		case 2:
-		case undefined:
-		default:
-			return '0b'+a.toString(2);
-		case 10:
-			return a.toString(10);
-		case 16:
-			return '0x'+a.toString(16);
-		case 8:
-			return '0o'+a.toString(8);
-		}
-	case 'undefined':
-		return 'undefined';
-	case 'function':
-	case 'boolean':
-		return a.toString();
-	}
-}
 
 function massive(name, fn, pairs, sradix, dradix) {
 	suite(name, function () {
 		for (var i = 0, n = pairs.length; i < n; i += 2)
 			(function (args, ret) {
-				test(fn.name+'('+ts(args, sradix)+') -> '+ts(ret, dradix)+'', function (done) {
+				test(fn.name+'('+json.js2str(args, sradix)+') -> '+json.js2str(ret, dradix)+'', function (done) {
 					assert.strictEqual(args instanceof Array ? fn.apply(null, args) : fn.call(null, args), ret);
 					done();
 				});
@@ -94,7 +19,7 @@ function massive_reversed(name, fn, pairs, sradix, dradix) {
 	suite(name, function () {
 		for (var i = 0, n = pairs.length; i < n; i += 2)
 			(function (args, ret) {
-				test(fn.name+'('+ts(args, sradix)+') -> '+ts(ret, dradix)+'', function (done) {
+				test(fn.name+'('+json.js2str(args, sradix)+') -> '+json.js2str(ret, dradix)+'', function (done) {
 					assert.strictEqual(args instanceof Array ? fn.apply(null, args) : fn.call(null, args), ret);
 					done();
 				});
@@ -107,42 +32,42 @@ suite('new Ejs() // default options', function () {
 	massive('parsing with <?=expression?>', function (text) {
 		return new Ejs().push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wrwr<?='5'?>werwer", "$.push('wrwr', '5'+'', 'werwer');\n",
-		"wrwr<?=888?>werwer", "$.push('wrwr', 888+'', 'werwer');\n",
-		"<?=888?>werwer", "$.push(888+'', 'werwer');\n",
-		"qweqwe<?=888?>", "$.push('qweqwe', 888+'');\n",
-		"<?=888?>", "$.push(888+'');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wrwr<?='5'?>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+('5')+'werwer';\n",
+		"wrwr<?=888?>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(888)+'werwer';\n",
+		"<?=888?>werwer", "\"use strict\";\nvar $ = \"\";\n$+=(888)+'werwer';\n",
+		"qweqwe<?=888?>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe'+(888);\n",
+		"<?=888?>", "\"use strict\";\nvar $ = \"\";\n$+=(888);\n"
 	]);
 
 	massive('parsing with <?.id()?>', function (text) {
 		return new Ejs().push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wrwr<?.fn()?>werwer", "$.push('wrwr', global.fn()+'', 'werwer');\n",
-		"wrwr<?.ooo('4')?>werwer", "$.push('wrwr', global.ooo('4')+'', 'werwer');\n",
-		"<?.ogo(4)?>werwer", "$.push(global.ogo(4)+'', 'werwer');\n",
-		"qweqwe<?.job(true)?>", "$.push('qweqwe', global.job(true)+'');\n",
-		"<?.just(1.1)?>", "$.push(global.just(1.1)+'');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wrwr<?.fn()?>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(global.fn())+'werwer';\n",
+		"wrwr<?.ooo('4')?>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(global.ooo('4'))+'werwer';\n",
+		"<?.ogo(4)?>werwer", "\"use strict\";\nvar $ = \"\";\n$+=(global.ogo(4))+'werwer';\n",
+		"qweqwe<?.job(true)?>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe'+(global.job(true));\n",
+		"<?.just(1.1)?>", "\"use strict\";\nvar $ = \"\";\n$+=(global.just(1.1));\n"
 	]);
 
 	massive('parsing with <? JS-CODE ?>', function (text) {
 		return new Ejs().push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wr'wr\n<? /* JS */ ?>werwer", "$.push('wr\\'wr\\n\\\n');\n/* JS */\n$.push('werwer');\n",
-		"wrwr<? while (1) {\n\t?>werwer<?\n}?>", "$.push('wrwr');\nwhile (1) {\n$.push('werwer');\n}\n",
-		"<? if (o) ?>werwer", "if (o)\n$.push('werwer');\n",
-		"qweqwe<? var q = 1; ?>", "$.push('qweqwe');\nvar q = 1;\n",
-		"<? /* only JS code */ \"use strict\"; ?>", "/* only JS code */ \"use strict\";\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wr'wr\n<? /* JS */ ?>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wr\\'wr\\n\\\n';\n/* JS */\n$+='werwer';\n",
+		"wrwr<? while (1) {\n\t?>werwer<?\n}?>", "\"use strict\";\nvar $ = \"\";\n$+='wrwr';\nwhile (1) {\n$+='werwer';\n}\n",
+		"<? if (o) ?>werwer", "\"use strict\";\nvar $ = \"\";\nif (o)\n$+='werwer';\n",
+		"qweqwe<? var q = 1; ?>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe';\nvar q = 1;\n",
+		"<? /* only JS code */ \"use strict\"; ?>", "\"use strict\";\nvar $ = \"\";\n/* only JS code */ \"use strict\";\n"
 	]);
 
 	massive('parsing JS ?>test<?', function (text) {
 		return new Ejs().push_js(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "wrwrwerwer\n",
-		"wrwrwerwer ?>just test<? js-code", "wrwrwerwer\n$.push('just test');\njs-code\n",
-		"wrwrwerwer ?>just test", "wrwrwerwer\n$.push('just test');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n",
+		"wrwrwerwer ?>just test<? js-code", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n$+='just test';\njs-code\n",
+		"wrwrwerwer ?>just test", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n$+='just test';\n"
 	]);
 
 	massive('is_ejs() method', function (text) {
@@ -211,42 +136,42 @@ suite('new Ejs({ open_str: "<%", close_str: "%>", global_id: "window" })', funct
 	massive('parsing with <%=expression%>', function (text) {
 		return new Ejs(o).push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wrwr<%='5'%>werwer", "$.push('wrwr', '5'+'', 'werwer');\n",
-		"wrwr<%=888%>werwer", "$.push('wrwr', 888+'', 'werwer');\n",
-		"<%=888%>werwer", "$.push(888+'', 'werwer');\n",
-		"qweqwe<%=888%>", "$.push('qweqwe', 888+'');\n",
-		"<%=888%>", "$.push(888+'');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wrwr<%='5'%>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+('5')+'werwer';\n",
+		"wrwr<%=888%>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(888)+'werwer';\n",
+		"<%=888%>werwer", "\"use strict\";\nvar $ = \"\";\n$+=(888)+'werwer';\n",
+		"qweqwe<%=888%>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe'+(888);\n",
+		"<%=888%>", "\"use strict\";\nvar $ = \"\";\n$+=(888);\n"
 	]);
 
 	massive('parsing with <%.id()%>', function (text) {
 		return new Ejs(o).push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wrwr<%.fn()%>werwer", "$.push('wrwr', window.fn()+'', 'werwer');\n",
-		"wrwr<%.ooo('4')%>werwer", "$.push('wrwr', window.ooo('4')+'', 'werwer');\n",
-		"<%.ogo(4)%>werwer", "$.push(window.ogo(4)+'', 'werwer');\n",
-		"qweqwe<%.job(true)%>", "$.push('qweqwe', window.job(true)+'');\n",
-		"<%.just(1.1)%>", "$.push(window.just(1.1)+'');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wrwr<%.fn()%>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(window.fn())+'werwer';\n",
+		"wrwr<%.ooo('4')%>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwr'+(window.ooo('4'))+'werwer';\n",
+		"<%.ogo(4)%>werwer", "\"use strict\";\nvar $ = \"\";\n$+=(window.ogo(4))+'werwer';\n",
+		"qweqwe<%.job(true)%>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe'+(window.job(true));\n",
+		"<%.just(1.1)%>", "\"use strict\";\nvar $ = \"\";\n$+=(window.just(1.1));\n"
 	]);
 
 	massive('parsing with <% JS-CODE %>', function (text) {
 		return new Ejs(o).push_ejs(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "$.push('wrwrwerwer');\n",
-		"wr'wr\n<% /* JS */ %>werwer", "$.push('wr\\'wr\\n\\\n');\n/* JS */\n$.push('werwer');\n",
-		"wrwr<% while (1) {\n\t%>werwer<%\n}%>", "$.push('wrwr');\nwhile (1) {\n$.push('werwer');\n}\n",
-		"<% if (o) %>werwer", "if (o)\n$.push('werwer');\n",
-		"qweqwe<% var q = 1; %>", "$.push('qweqwe');\nvar q = 1;\n",
-		"<% /* only JS code */ \"use strict\"; %>", "/* only JS code */ \"use strict\";\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\n$+='wrwrwerwer';\n",
+		"wr'wr\n<% /* JS */ %>werwer", "\"use strict\";\nvar $ = \"\";\n$+='wr\\'wr\\n\\\n';\n/* JS */\n$+='werwer';\n",
+		"wrwr<% while (1) {\n\t%>werwer<%\n}%>", "\"use strict\";\nvar $ = \"\";\n$+='wrwr';\nwhile (1) {\n$+='werwer';\n}\n",
+		"<% if (o) %>werwer", "\"use strict\";\nvar $ = \"\";\nif (o)\n$+='werwer';\n",
+		"qweqwe<% var q = 1; %>", "\"use strict\";\nvar $ = \"\";\n$+='qweqwe';\nvar q = 1;\n",
+		"<% /* only JS code */ \"use strict\"; %>", "\"use strict\";\nvar $ = \"\";\n/* only JS code */ \"use strict\";\n"
 	]);
 
 	massive('parsing JS %>test<%', function (text) {
 		return new Ejs(o).push_js(text).push_code().listing();
 	}, [
-		"wrwrwerwer", "wrwrwerwer\n",
-		"wrwrwerwer %>just test<% js-code", "wrwrwerwer\n$.push('just test');\njs-code\n",
-		"wrwrwerwer %>just test", "wrwrwerwer\n$.push('just test');\n"
+		"wrwrwerwer", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n",
+		"wrwrwerwer %>just test<% js-code", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n$+='just test';\njs-code\n",
+		"wrwrwerwer %>just test", "\"use strict\";\nvar $ = \"\";\nwrwrwerwer\n$+='just test';\n"
 	]);
 
 	massive('is_ejs() method', function (text) {
@@ -304,4 +229,18 @@ suite('new Ejs({ open_str: "<%", close_str: "%>", global_id: "window" })', funct
 		"wrwr-<% for (var c = 4; c; --c) {\n\t%>(<%=c%> werwer)<%\n}%>", "wrwr-(4 werwer)(3 werwer)(2 werwer)(1 werwer)"
 	]);
 
+});
+
+suite('new Ejs({ open_str: "<%", close_str: "%>", global_id: "window" })', function () {
+	test('compile crash test', function (done) {
+		var text = "ert <? erert] ?> wrtret",
+		    src = "\"use strict\";\nvar $ = \"\";\n$+='ert ';\nerert]\n$+=' wrtret';\n;return $;\n";
+		try {
+			Ejs.compile(text, "one,two");
+			done(Error('not failed!'));
+		} catch (e) {
+			assert.strictEqual(e.source, src);
+			done();
+		}
+	});
 });
